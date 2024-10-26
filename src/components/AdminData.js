5b2713aec0be76b6b4d02576a345e28e6c5f107c
Ankita <ankitapatil24042001@gmail.com>
2024-10-26 19:49:02 +0530

    first commit

--- src/components/AdminData.js
+++ src/components/AdminData.js
@@ -156,7 +156,7 @@ function AdminData() {
   return (
     <>
       <Navbar />
-      <div className="p-6 bg-gray-200 min-h-screen xsm:w-[280px] xl:w-full overflow-auto">
+      <div className="p-6 bg-gray-200 min-h-screen w-full overflow-auto">
         <h1 className="text-3xl font-bold text-gray-800 mb-4">Admin Data</h1>
         {data.length > 0 ? (
           <div className="overflow-hidden border-b border-gray-200 shadow-md rounded-lg">
